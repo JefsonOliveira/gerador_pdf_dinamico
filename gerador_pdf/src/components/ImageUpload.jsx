97811645207a980dac3c366c954f3c7d335b1f50
jefsonoliveira <kaironoliveira16@gmail.com>
2024-06-25 12:18:49 -0300

    feat: Setup inicial do projeto -Gerador de PDF Dinamico- -ReactJs- -HoraDeCodar-

---
+++ gerador_pdf/src/components/ImageUpload.jsx
@@ -0,0 +1,12 @@
+const ImageUpload = () => {
+  return (
+    <div className="imageUpload">
+      <label className="configLabel">
+        Upload Image:
+        <input type="file" accept="image/" className="fileInput" />
+      </label>
+    </div>
+  );
+};
+
+export default ImageUpload;
